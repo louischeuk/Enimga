fd1554b890d74be7175d4c57dd55a0700314d9bd
Louis Cheuk <louischeuk@1679-MBP-19.local>
2020-11-09 08:11:57 +0800

    .

--- rotor.h
+++ rotor.h
@@ -12,24 +12,24 @@
 // class Enigma;
 
 class Rotor {
-    // int alphabet_map[2][26];
-    int alphabet_map[26];
+	// int alphabet_map[2][26];
+	int alphabet_map[26];
 public:
-    int code;
-    int top_pos;
-    int number_of_notch;
-    int *notch_pos; // should move to private
+	int code;
+	int top_pos;
+	int number_of_notch;
+	int *notch_pos; // should move to private
 
-    Rotor(const char* filename);
-    void check_config(const char* filename);
-    void implement_config(const char* filename);
+	Rotor(const char* filename);
+	void check_config(const char* filename);
+	void implement_config(const char* filename);
 
-    int w(const int &letter);
-    int w_inverse(const int &letter);
-    int map_r_to_l(const int &letter);
-    int map_l_to_r(const int &letter);
-    void rotate();
-    friend int set_starting_pos(Rotor **rotor, int number_of_rotors, const char *filename); // number_of_rotors is from the enimga class,
+	int w(const int &letter);
+	int w_inverse(const int &letter);
+	int map_r_to_l(const int &letter);
+	int map_l_to_r(const int &letter);
+	void rotate();
+	friend int set_starting_pos(Rotor **rotor, int number_of_rotors, const char *filename); // number_of_rotors is from the enimga class,
 };
 
 #endif
